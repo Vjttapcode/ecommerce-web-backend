77373f8fdb4718a9448c4a477fdbcbc0008e809c
Vjttapcode <ducvn180@gmail.com>
2025-12-02 15:03:03 +0700

    update: secure backend

--- src/main/java/com/example/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/example/ecommerce/config/SecurityConfiguration.java
@@ -5,38 +5,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        //protect endpoint /api/orders
-        http.authorizeHttpRequests(requests ->
-                        requests
-                                .requestMatchers("/api/orders/**")
-                                .authenticated()
-                                .anyRequest().permitAll())
-                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
-
-        // + CORS filters
-        http.cors(Customizer.withDefaults());
-
-        // + content negotiation strategy
-        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
-
-        // + non-empty response body for 401 (more friendly)
-        Okta.configureResourceServer401ResponseBody(http);
-
-        // we are not using Cookies for session tracking >> disable CSRF
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/api/orders/**")
+                        .authenticated().anyRequest().permitAll())
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()))
+                .cors(withDefaults());
+        //Fix 403 error for api/checkout/purchase (post request)
         http.csrf(AbstractHttpConfigurer::disable);
-
+        // add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+        // force a non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
         return http.build();
     }
-
 }
